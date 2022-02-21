4cd1e893387acbf59c2705c6b7c7390e7cc9ee3e
goncy <gonzalo.pozzo4@gmail.com>
2022-02-21 10:38:28 -0300

    Update links

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import {Container, Text} from "@chakra-ui/react";
+import {Text} from "@chakra-ui/react";
 
 import Link from "../ui/controls/Link";
 
@@ -8,24 +8,16 @@ const NotFoundPage: React.VFC = () => {
   return (
     <>
       <Head>
-        <title>Communcy | not found</title>
-        <meta content="Goncy community featured profiles" name="description" />
-        <meta content="Communcy | not found" name="title" />
+        <title>Communcy | 404</title>
+        <meta content="Communcy | 404" name="title" />
       </Head>
-      <Container maxWidth="container.md" paddingY={6}>
-        <Text borderRadius="lg" layerStyle="card" padding={6} textStyle="soft"
-        >
-          This profile was not found, you can go to the{" "}
-          <Link color="primary.500" href="/">
-            index page
-          </Link>{" "}
-          to see a list of all the profiles or you can propose this profile on the{" "}
-          <Link isExternal color="primary.500" href="https://discord.gonzalopozzo.com">
-            discord server
-          </Link>
-          .
-        </Text>
-      </Container>
+      <Text fontSize="xl" margin="auto" padding={6} textAlign="center" textStyle="soft">
+        Este perfil no existe, podes ver los perfiles disponibles en la{" "}
+        <Link href="/" textStyle="link">
+          página principal
+        </Link>
+        .
+      </Text>
     </>
   );
 };
